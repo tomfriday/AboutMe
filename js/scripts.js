9ebbf8f5e675a3ecc852700d938fc9021dcf4417
piatunio <piatuniotomasz@gmail.com>
2017-09-21 10:43:54 +0200

    .

--- js/scripts.js
+++ js/scripts.js
@@ -16,22 +16,31 @@ $(function () {
         if ($('.aboutUs').isInViewport()) {
             $('.aboutUs').addClass('animated');
             $('.aboutUs').addClass('fadeIn');
+            $('.aboutUs h2').addClass('animated');
+            $('.aboutUs h2').addClass('fadeInDown');
         }
         if ($('.services').isInViewport()) {
             $('.services').addClass('animated');
             $('.services').addClass('fadeIn');
+            $('.services h2').addClass('animated');
+            $('.services h2').addClass('fadeInDown');
         }
         if ($('.WhatWeDo').isInViewport()) {
             $('.WhatWeDo').addClass('animated');
             $('.WhatWeDo').addClass('fadeIn');
+            $('.WhatWeDo h2').addClass('animated');
+            $('.WhatWeDo h2').addClass('fadeInDown');
         }
         if ($('.gallery').isInViewport()) {
             $('.gallery').addClass('animated');
             $('.gallery').addClass('fadeIn');
+
         }
         if ($('.contact').isInViewport()) {
             $('.contact').addClass('animated');
             $('.contact').addClass('fadeInDown');
+            $('.contact h2').addClass('animated');
+            $('.contact h2').addClass('fadeInDown');
         }
         var y = $(this).scrollTop();
         if (y > 100) {
